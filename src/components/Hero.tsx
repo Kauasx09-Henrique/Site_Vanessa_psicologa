b6d37656f6932201b2fe839ce3cc7e64750583ad
Kauã Henrique <kauaheenriquesx09@gmail.com>
2025-07-02 13:42:57 -0300

    Fix

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -5,17 +5,7 @@ import './style/Hero.css'; // Lembre-se que criamos um CSS para ele
 const Hero = () => {
   return (
     <section className="hero-container">
-      <div className="hero-content">
-        <h1 className="hero-headline">
-          Cuidado, acolhimento e transformação
-        </h1>
-        <p className="hero-subheadline">
-          Psicoterapia online e presencial para o seu bem-estar. Encontre um espaço seguro para se redescobrir.
-        </p>
-        <a href="#contato" className="hero-cta-button">
-    
-        </a>
-      </div>
+     
     </section>
   );
 };
